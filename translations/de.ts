ae8616e6c5925518cc24811eb0b19b3a59ec4a4d
nephros <nemo@JollaC2>
2025-08-02 17:06:28 +0200

    SFOS I18N: another approach

---
+++ translations/de.ts
@@ -0,0 +1,279 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+<context>
+    <name></name>
+    <message>
+        <source>Load image</source>
+        <translation type="vanished">Bild laden</translation>
+    </message>
+    <message>
+        <source>Plants</source>
+        <translation type="vanished">Pflanzen</translation>
+    </message>
+    <message>
+        <source>Identification failed</source>
+        <translation type="vanished">Identifizierung fehlgeschlagen</translation>
+    </message>
+    <message>
+        <source>Failed to send identification request to Pl@ntNet (%1).</source>
+        <translation type="vanished">Identifikationsanfrage an Pl@ntNet (%1) konnte nicht gesendet werden.</translation>
+    </message>
+    <message>
+        <source>Failed to init storage directory</source>
+        <translation type="vanished">Speicherverzeichnis konnte nicht initialisiert werden</translation>
+    </message>
+    <message>
+        <source>Storage directory could not be initialized (%1).</source>
+        <translation type="vanished">Das Speicherverzeichnis konnte nicht initialisiert werden (%1).</translation>
+    </message>
+    <message>
+        <source>No plants identified yet</source>
+        <translation type="vanished">Noch keine Pflanzen identifiziert</translation>
+    </message>
+    <message>
+        <source>New identification</source>
+        <translation type="vanished">Neue Identifizierung</translation>
+    </message>
+    <message>
+        <source>API Key missing</source>
+        <translation type="vanished">API-Schlüssel fehlt</translation>
+    </message>
+    <message>
+        <source>The Pl@ntNet API-Key has not been configured yet. Without this, the app will not work.</source>
+        <translation type="vanished">Der Pl@ntNet API-Key wurde noch nicht konfiguriert. Ohne diesen wird die App nicht funktionieren.</translation>
+    </message>
+    <message>
+        <source>Delete plant?</source>
+        <translation type="vanished">Pflanze löschen?</translation>
+    </message>
+    <message>
+        <source>Shall the plant &apos;%1&apos; be deleted? This operation can not be undone.</source>
+        <translation type="vanished">Soll die Pflanze &apos;%1&apos; gelöscht werden? Dieser Vorgang kann nicht rückgängig gemacht werden.</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation type="vanished">Löschen</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="vanished">Abbrechen</translation>
+    </message>
+    <message>
+        <source>Deleting plant failed</source>
+        <translation type="vanished">Löschung fehlgeschlagen</translation>
+    </message>
+    <message>
+        <source>Plant could not be deleted (%1).</source>
+        <translation type="vanished">Pflanze konnte nicht gelöscht werden (%1).</translation>
+    </message>
+    <message>
+        <source>1 identified plant</source>
+        <translation type="vanished">1 identifizierte Pflanze</translation>
+    </message>
+    <message>
+        <source>%1 identified plants</source>
+        <translation type="vanished">%1 identifizierte Pflanzen</translation>
+    </message>
+    <message>
+        <source>Plant details</source>
+        <translation type="vanished">Pflanzendetails</translation>
+    </message>
+    <message>
+        <source>Add up to 5 images for identification. The images must be of the same plant. The more images are provided, the better the identification result will be.</source>
+        <translation type="vanished">Bis zu 5 Fotos können zur Identifizierung verwendet werden. Die Fotos müssen von der gleichen Pflanze sein. Je mehr Fotos verwendet werden, desto genauer wird das Analyseergebnis sein.</translation>
+    </message>
+    <message>
+        <source>Pl@ntNet recommends images with the smaller side larger than 600px and smaller than 2000px. Ideally a square image zoomed on the organ around 1280x1280px.</source>
+        <translation type="vanished">Pl@ntNet empfiehlt Fotos mit der kürzeren Seite mehr als 600px und weniger als 2000px. Idealerweise ein Quadrat mit Fokus auf die Pflanze von etwa 1280x1280px.</translation>
+    </message>
+    <message>
+        <source>Identify</source>
+        <translation type="vanished">Identifizieren</translation>
+    </message>
+    <message>
+        <source>Identification results</source>
+        <translation type="vanished">Identifizierungsergebnis</translation>
+    </message>
+    <message>
+        <source>Saving result failed</source>
+        <translation type="vanished">Speichern fehlgeschlagen</translation>
+    </message>
+    <message>
+        <source>Result could not be saved (%1).</source>
+        <translation type="vanished">Ergebnis konnte nicht gespeichert werden (%1).</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation type="vanished">Einstellungen</translation>
+    </message>
+    <message>
+        <source>Pl@ntNet API key</source>
+        <translation type="vanished">Pl@ntNet API-Schlüssel</translation>
+    </message>
+    <message>
+        <source>In order to use the Pl@ntNet plant identification service, it is necessary to register at their website as developer and obtain an API-Key. This key needs to be configured within this app.
+
+Please visit &lt;a href=&quot;https://my.plantnet.org/signup&quot;&gt;https://my.plantnet.org/signup&lt;/a&gt; and create a developer account. Afterwards visit &lt;a href=&quot;https://my.plantnet.org/account&quot;&gt;https://my.plantnet.org/account&lt;/a&gt; and click the eye-symbol at the very top (&quot;my API key&quot;) to show the API-Key. Copy this key and paste it into the below text input field.</source>
+        <translation type="vanished">Um den Pflanzen-Identifizierungsdienst von Pl@ntNet zu nutzen, ist es notwendig, auf der Webseite von Pl@ntNet ein Entwicklerkonto zu erstellen um einen API-Schlüssel zu erhalten. Dieser API-Schlüssel muss in der App hinterlegt werden.
+
+Bitte ein Entwickler-Konto auf &lt;a href=&quot;https://my.plantnet.org/signup&quot;&gt;https://my.plantnet.org/signup&lt;/a&gt; erstellen und anschließend unter &lt;a href=&quot;https://my.plantnet.org/account&quot;&gt;https://my.plantnet.org/account&lt;/a&gt; ganz oben mit dem &quot;Augen-Symbol&quot; den API-Schlüssel anzeigen lassen. Dieser kann dann in das unten stehende Eingabefeld kopiert werden.</translation>
+    </message>
+    <message>
+        <source>API-Key:</source>
+        <translation type="vanished">API-Schlüssel:</translation>
+    </message>
+    <message>
+        <source>Enter API-Key</source>
+        <translation type="vanished">API-Schlüssel eingeben</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation type="vanished">Speichern</translation>
+    </message>
+    <message>
+        <source>Keep display on while using the app</source>
+        <translation type="vanished">Display beim Verwenden der App eingeschaltet lassen</translation>
+    </message>
+    <message>
+        <source>Okay</source>
+        <translation type="vanished">Okay</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation type="vanished">Schließen</translation>
+    </message>
+    <message>
+        <source>Select plant part</source>
+        <translation type="vanished">Pflanzenteil auswählen</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation type="vanished">Auswählen</translation>
+    </message>
+    <message>
+        <source>This app uses the online service of Pl@ntNet (plantnet.org) to identify plants. Photos will be uploaded to Pl@ntNet for recognition, and the result will show a probability of correctness. This means, results provided by the service could be wrong, or some plants can maybe not be identified at all.</source>
+        <translation type="vanished">Diese App nutzt den Online-Dienst von Pl@ntNet (plantnet.org), um Pflanzen zu identifizieren. Die Fotos werden zur Erkennung auf Pl@ntNet hochgeladen, und das Ergebnis zeigt eine Wahrscheinlichkeit für die Richtigkeit an. Das bedeutet, dass die vom Dienst gelieferten Ergebnisse falsch sein können, oder dass einige Pflanzen vielleicht gar nicht identifiziert werden können.</translation>
+    </message>
+    <message>
+        <source>Accept</source>
+        <translation type="vanished">Annehmen</translation>
+    </message>
+    <message>
+        <source>Plant is being identified, please wait.</source>
+        <translation type="vanished">Pflanze wird identifiziert, bitte warten.</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation type="vanished">Name</translation>
+    </message>
+    <message>
+        <source>Common names</source>
+        <translation type="vanished">Gebräuchliche Namen</translation>
+    </message>
+    <message>
+        <source>Save this result</source>
+        <translation type="vanished">Dieses Ergebnis speichern</translation>
+    </message>
+    <message>
+        <source>Tap to add new image</source>
+        <translation type="vanished">Tippe um Foto hinzuzufügen</translation>
+    </message>
+    <message>
+        <source>Organ</source>
+        <translation type="vanished">Pflanzenteil</translation>
+    </message>
+    <message>
+        <source>Auto</source>
+        <translation type="vanished">Automatisch</translation>
+    </message>
+    <message>
+        <source>Leaf</source>
+        <translation type="vanished">Blatt</translation>
+    </message>
+    <message>
+        <source>Flower</source>
+        <translation type="vanished">Blüte</translation>
+    </message>
+    <message>
+        <source>Fruit</source>
+        <translation type="vanished">Frucht</translation>
+    </message>
+    <message>
+        <source>Bark</source>
+        <translation type="vanished">Rinde</translation>
+    </message>
+    <message>
+        <source>No source images in identification result</source>
+        <translation type="vanished">Keine Quell-Fotos im Identifizierungsergebnis</translation>
+    </message>
+    <message>
+        <source>Own photo</source>
+        <translation type="vanished">Eigenes Foto</translation>
+    </message>
+    <message>
+        <source>Failed to save source images for plant: </source>
+        <translation type="vanished">Quell-Fotos der Pflanze konnten nicht speichern:</translation>
+    </message>
+    <message>
+        <source>Failed to save plant JSON: </source>
+        <translation type="vanished">JSON-Datei konnte nicht gespeichert werden:</translation>
+    </message>
+    <message>
+        <source>Failed to open plant JSON: </source>
+        <translation type="vanished">JSON-Datei konnte nicht geöffnet werden:</translation>
+    </message>
+    <message>
+        <source>Failed to open plant JSON #2: </source>
+        <translation type="vanished">JSON-Datei konnte nicht geöffnet werden #2:</translation>
+    </message>
+    <message>
+        <source>Failed to read plant JSON: </source>
+        <translation type="vanished">JSON-Datei konnte nicht gelesen werden:</translation>
+    </message>
+    <message>
+        <source>Unexpected/malformed plant JSON #1</source>
+        <translation type="vanished">Unerwartetes/ungültiges JSON-Format #1</translation>
+    </message>
+    <message>
+        <source>Unexpected/malformed plant JSON #2</source>
+        <translation type="vanished">Unerwartetes/ungültiges JSON-Format #2</translation>
+    </message>
+    <message>
+        <source>Unexpected/malformed plant JSON #3</source>
+        <translation type="vanished">Unerwartetes/ungültiges JSON-Format #3</translation>
+    </message>
+    <message>
+        <source>Failed to open plant image: </source>
+        <translation type="vanished">Pflanzen-Foto konnte nicht geöffnet werden:</translation>
+    </message>
+    <message>
+        <source>Failed to determine writable app data storage location</source>
+        <translation type="vanished">Fehler beim Ermitteln des Speicherorts für App-Daten</translation>
+    </message>
+    <message>
+        <source>App data storage location inaccessible</source>
+        <translation type="vanished">Zugriff auf App-Datenspeicherort nicht möglich</translation>
+    </message>
+    <message>
+        <source>can&apos;t create subfolder:</source>
+        <translation type="vanished">Unterordner kann nicht angelegt werden:</translation>
+    </message>
+    <message>
+        <source>Failed to save plant</source>
+        <translation type="vanished">Pflanze konnte nicht gespeichert werden</translation>
+    </message>
+    <message>
+        <source>Failed to delete plant (plant unknown)</source>
+        <translation type="vanished">Pflanze konnte nicht gelöscht werden (Pflanze unbekannt)</translation>
+    </message>
+    <message>
+        <source>Failed to delete plant from storage directory (%1)</source>
+        <translation type="vanished">Pflanze konnte nicht aus dem Speicherordner gelöscht werden (%1)</translation>
+    </message>
+    <message>
+        <source>Failed to delete plant image from storage directory (%1)</source>
+        <translation type="vanished">Pflanzen-Foto konnte nicht aus dem Speicherordner gelöscht werden (%1)</translation>
+    </message>
+</context>
+</TS>
