8cfa6ecc4917e9ff6d043f9ad09d36abe218fd08
nephros <nemo@JollaC2>
2025-08-04 08:55:03 +0200

    Language conversion: remove locations

--- lang_conv.sh
+++ lang_conv.sh
@@ -5,7 +5,7 @@ printf '<RCC><qresource prefix="/i18n">\n' >> translations/translations.qrc
 for f in po/*po; do
   lang=$(basename $f .po)
   echo $lang
-  lconvert -target-language $lang -if po -i $f -of ts -o translations/${lang}.ts
+  lconvert -target-language $lang -locations none -if po -i $f -of ts -o translations/${lang}.ts
   lrelease translations/${lang}.ts -qm translations/harbour-plants_${lang}.qm
   printf '<file>%s</file>\n' harbour-plants_${lang}.qm >> translations/translations.qrc
 done
