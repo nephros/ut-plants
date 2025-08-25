a23166acd48d824f6655eb6dd7415416d5c67ca5
Åke Engelbrektson <eson57@users.noreply.github.com>
2025-08-25 11:35:13 +0200

    Create sv.ts
    Add Swedish translation.
    Don't know if you want this or the po file, so I'll go for ts for now.  ;-)

---
+++ translations/sv.ts
@@ -0,0 +1,377 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>Dialogs</name>
+    <message>
+        <source>Okay</source>
+        <translation>Okay</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Stäng</translation>
+    </message>
+    <message>
+        <source>Select plant part</source>
+        <translation>Välj växtdel</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation>Välj</translation>
+    </message>
+    <message>
+        <source>Failed to init storage directory</source>
+        <translation>Kunde inte initiera lagringsplats</translation>
+    </message>
+    <message>
+        <source>Storage directory could not be initialized (%1).</source>
+        <translation>Lagringsplatsen kunde inte initieras (%1).</translation>
+    </message>
+</context>
+<context>
+    <name>Disclaimer</name>
+    <message>
+        <source>This app uses the online service of Pl@ntNet (plantnet.org) to identify plants. Photos will be uploaded to Pl@ntNet for recognition, and the result will show a probability of correctness. This means, results provided by the service could be wrong, or some plants can maybe not be identified at all.</source>
+        <translation>Denna app använder den online-tjänst som Pl@ntNet (plantnet.org) erbjuder för att identifiera växter. Bilder kommer att laddas upp till Pl@ntNet för igenkänning, och resultatet kommer att visa ett sannolikt korrekt resultat. Detta betyder att resultaten som tillhandahålls av tjänsten kan vara felaktiga, eller att vissa växter kanske inte kan identifieras alls.</translation>
+    </message>
+    <message>
+        <source>Accept</source>
+        <translation>Godkänn</translation>
+    </message>
+</context>
+<context>
+    <name>ErrorDialog</name>
+    <message>
+        <source>Close</source>
+        <translation>Stäng</translation>
+    </message>
+</context>
+<context>
+    <name>ImportPage</name>
+    <message>
+        <source>Load image</source>
+        <translation>Läs in bild</translation>
+    </message>
+</context>
+<context>
+    <name>LoadingScreen</name>
+    <message>
+        <source>Plant is being identified, please wait.</source>
+        <translation>Växten identifieras, vänta.</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Failed to init storage directory</source>
+        <translation>Kunde inte initiera lagringsplats</translation>
+    </message>
+    <message>
+        <source>Storage directory could not be initialized (%1).</source>
+        <translation>Lagringsplats kunde inte initieras (%1).</translation>
+    </message>
+    <message>
+        <source>Plants</source>
+        <translation>Växter</translation>
+    </message>
+    <message>
+        <source>Plant is being identified, please wait.</source>
+        <translation>Växten identifieras, vänta!</translation>
+    </message>
+    <message>
+        <source>1 identified plant</source>
+        <translation>1 identifierad växt</translation>
+    </message>
+    <message>
+        <source>%1 identified plants</source>
+        <translation>%1 identifierade växter</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Ta borrt</translation>
+    </message>
+    <message>
+        <source>Deleting plant failed</source>
+        <translation>Kunde inte ta bort växten</translation>
+    </message>
+    <message>
+        <source>Plant could not be deleted (%1).</source>
+        <translation>Växten kunde inte tas bort (%1).</translation>
+    </message>
+    <message>
+        <source>No plants identified yet</source>
+        <translation>Inga växter identifierade ännu</translation>
+    </message>
+    <message>
+        <source>Pull down to start a new identification</source>
+        <translation>Dra neråt för att starta en ny identifiering</translation>
+    </message>
+    <message>
+        <source>New identification</source>
+        <translation>Ny identifiering</translation>
+    </message>
+    <message>
+        <source>API Key missing</source>
+        <translation>API-nyckel saknas</translation>
+    </message>
+    <message>
+        <source>The Pl@ntNet API-Key has not been configured yet. Without this, the app will not work.</source>
+        <translation>Pl@ntNets API-nyckel har ännu inte konfigurerats. Utan denna kommer appen inte att fungera.</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+</context>
+<context>
+    <name>PickerDialog</name>
+    <message>
+        <source>Select plant part</source>
+        <translation>Välj växtdel</translation>
+    </message>
+</context>
+<context>
+    <name>PlantCard</name>
+    <message>
+        <source>Name</source>
+        <translation>Namn</translation>
+    </message>
+    <message>
+        <source>Genus</source>
+        <translation>Släkte</translation>
+    </message>
+    <message>
+        <source>Common names</source>
+        <translation>Vanliga namn</translation>
+    </message>
+</context>
+<context>
+    <name>PlantItem</name>
+    <message>
+        <source>Organ</source>
+        <translation>Organ</translation>
+    </message>
+</context>
+<context>
+    <name>PlantPage</name>
+    <message>
+        <source>Plant details</source>
+        <translation>Växtdetaljer</translation>
+    </message>
+    <message>
+        <source>Copy names to Clipboard</source>
+        <translation>Kopiera namn till urklipp</translation>
+    </message>
+    <message>
+        <source>Copy species to Clipboard</source>
+        <translation>Kopiera arter till urklipp</translation>
+    </message>
+    <message>
+        <source>Copy family to Clipboard</source>
+        <translation>Kopiera släkte till urklipp</translation>
+    </message>
+    <message>
+        <source>Search on Wikipedia</source>
+        <translation>Sök på Wikipedia</translation>
+    </message>
+</context>
+<context>
+    <name>PlantUtils</name>
+    <message>
+        <source>Auto</source>
+        <translation>Auto</translation>
+    </message>
+    <message>
+        <source>Leaf</source>
+        <translation>Löv</translation>
+    </message>
+    <message>
+        <source>Flower</source>
+        <translation>Blomma</translation>
+    </message>
+    <message>
+        <source>Fruit</source>
+        <translation>Frukt</translation>
+    </message>
+    <message>
+        <source>Bark</source>
+        <translation>Stam</translation>
+    </message>
+</context>
+<context>
+    <name>RequestPage</name>
+    <message>
+        <source>New identification</source>
+        <translation>Ny identifiering</translation>
+    </message>
+    <message>
+        <source>Add up to 5 images for identification. The images must be of the same plant. The more images are provided, the better the identification result will be.</source>
+        <translation>Lägg in upp till 5 bilder för identifiering. Bilderna måste vara av samma planta. Ju fler bilder, desto bättre identifieringsresultat.</translation>
+    </message>
+    <message>
+        <source>Pl@ntNet recommends images with the smaller side larger than 600px and smaller than 2000px. Ideally a square image zoomed on the organ around 1280x1280px.</source>
+        <translation>Pl@ntNet rekommenderar bilder med den mindre sidan större än 600px och mindre än 2000px. Helst en fyrkantig bild zoomad på organet runt 1280x1280px.</translation>
+    </message>
+    <message>
+        <source>Select plant part</source>
+        <translation>Välj växtdel</translation>
+    </message>
+    <message>
+        <source>Remove image</source>
+        <translation>Ta bort bild</translation>
+    </message>
+    <message>
+        <source>Add Images</source>
+        <translation>Lägg till bilder</translation>
+    </message>
+    <message>
+        <source>Can not add more than 5 images</source>
+        <translation>Kan inte lägga till mer än 5 bilder</translation>
+    </message>
+    <message>
+        <source>Identify</source>
+        <translation>Identifiera</translation>
+    </message>
+    <message>
+        <source>Remove All</source>
+        <translation>Ta bort alla</translation>
+    </message>
+    <message>
+        <source>Cleared</source>
+        <translation>Rensat</translation>
+    </message>
+    <message>
+        <source>No images</source>
+        <translation>Inga bilder</translation>
+    </message>
+    <message>
+        <source>Pull down to add new images</source>
+        <translation>Dra neråt för att lägga till nya bilder</translation>
+    </message>
+</context>
+<context>
+    <name>ResultsPage</name>
+    <message>
+        <source>Identification results: </source>
+        <translation>Identifieringsresultat: </translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Spara</translation>
+    </message>
+    <message>
+        <source>Back</source>
+        <translation>Tillbaka</translation>
+    </message>
+    <message>
+        <source>Saving result failed</source>
+        <translation>Kunde inte spara resultat</translation>
+    </message>
+    <message>
+        <source>Result could not be saved (%1).</source>
+        <translation>Resultatet kunde inte sparas (%1).</translation>
+    </message>
+    <message>
+        <source>%1/%2 results</source>
+        <translation>%1/%2 resultat</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>Pl@ntNet API key</source>
+        <translation>Pl@ntNet API-nyckel</translation>
+    </message>
+    <message>
+        <source>In order to use the Pl@ntNet plant identification service, it is necessary to register at their website as developer and obtain an API-Key. This key needs to be configured within this app.
+
+Please visit &lt;a href=&quot;https://my.plantnet.org/signup&quot;&gt;https://my.plantnet.org/signup&lt;/a&gt; and create a developer account. Afterwards visit &lt;a href=&quot;https://my.plantnet.org/account&quot;&gt;https://my.plantnet.org/account&lt;/a&gt; and click the eye-symbol at the very top (&quot;my API key&quot;) to show the API-Key. Copy this key and paste it into the below text input field.</source>
+        <translation>För att använda Pl@ntNet:s växtidentifieringstjänst är det nödvändigt att registrera sig på deras webbplats som utvecklare och få en API-nyckel. Denna nyckel måste konfigureras i den här appen.
+
+Besök &lt;a href=&quot;https://my.plantnet.org/signup&quot;&gt;https://my.plantnet.org/signup&lt;/a&gt; och skapa ett utvecklarkonto. Efteråt, besök &lt;a href=&quot;https://my.plantnet.org/account&quot;&gt;https://my.plantnet.org/account&lt;/a&gt; och klicka på ögonsymbolen längst upp (&quot;My API key&quot;) för att visa API-nyckeln. Kopiera denna nyckel och klistra in den i fältet nedan.</translation>
+    </message>
+    <message>
+        <source>API-Key:</source>
+        <translation>API-nyckel:</translation>
+    </message>
+    <message>
+        <source>New API Key</source>
+        <translation>Ny API-nyckel</translation>
+    </message>
+    <message>
+        <source>Enter API-Key</source>
+        <translation>Ange API-nyckel</translation>
+    </message>
+    <message>
+        <source>The value will be saved on Enter.</source>
+        <translation>Värdet kommer att sparas med Retur.</translation>
+    </message>
+    <message>
+        <source>API Key already stored. Edit to update</source>
+        <translation>API-nyckel redan lagrad. Redigera för att uppdatera.</translation>
+    </message>
+    <message>
+        <source>No API Key stored</source>
+        <translation>Ingen API-nyckel lagrad</translation>
+    </message>
+    <message>
+        <source>Identification:</source>
+        <translation>Indentifiering:</translation>
+    </message>
+    <message>
+        <source>Result Language</source>
+        <translation>Resultatspråk</translation>
+    </message>
+    <message>
+        <source>The language to use for Identification results. Default is to use the current Locale, or English if not available.</source>
+        <translation>Språket som ska användas för identifieringsresultat. Standard är att använda det aktuella systemspråket, eller engelska om det inte finns tillgängligt.</translation>
+    </message>
+    <message>
+        <source>Number of results</source>
+        <translation>Antal resultat</translation>
+    </message>
+    <message>
+        <source>App:</source>
+        <translation>App:</translation>
+    </message>
+    <message>
+        <source>Prevent device sleep on pending request</source>
+        <translation>Förhindra att enheten släcks  vid pågående förfrågan.</translation>
+    </message>
+    <message>
+        <source>If enabled, device will not go into sleep mode while waiting on an identification result.</source>
+        <translation>Om det är aktiverat kommer enheten inte att gå in i viloläge när den väntar på ett identifieringsresultat.</translation>
+    </message>
+    <message>
+        <source>Region or Project</source>
+        <translation>Region eller projekt</translation>
+    </message>
+    <message>
+        <source>The Region or Project to use for Identification results. Default is to use all available.</source>
+        <translation>Regionen eller projektet som ska användas för identifieringsresultat. Standard är att använda alla tillgängliga.</translation>
+    </message>
+    <message>
+        <source>species</source>
+        <translation>arter</translation>
+    </message>
+</context>
+<context>
+    <name>harbour-plants</name>
+    <message>
+        <source>Identification failed</source>
+        <translation>Identifieringen misslyckades</translation>
+    </message>
+    <message>
+        <source>Failed to send identification request to Pl@ntNet (%1).</source>
+        <translation>Kunde inte inte skicka identifieringsförfrågan till Pl@ntNet (%1).</translation>
+    </message>
+</context>
+</TS>
